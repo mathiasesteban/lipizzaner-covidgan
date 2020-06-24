f5256de6843b1546ae4b39f61da4e1a5bb4cb5e5
jamaltoutouh <jamaltoutouh@gmail.com>
2020-06-24 18:40:25 -0400

    New convolutional networks 28x28, 64x64, and 128x128

--- src/networks/competetive_net.py
+++ src/networks/competetive_net.py
@@ -185,8 +185,6 @@ class GeneratorNetCovid(CompetetiveNet):
         z = noise(batch_size, self.data_size)
 
         fake_images = self.net(z)
-        print('Fake iamges: {}'.format(fake_images.size()))
-        print('D network: {}'.format(opponent.net))
         outputs = opponent.net(fake_images).view(-1)
 
         return self.loss_function(outputs, real_labels), fake_images, None
@@ -196,7 +194,6 @@ class GeneratorNetCovid(CompetetiveNet):
 class DiscriminatorNetCovid(CompetetiveNet):
     def __init__(self, loss_function, net, data_size, optimize_bias=True, image_length=28, image_width=28):
         CompetetiveNet.__init__(self, loss_function, net, data_size, optimize_bias=optimize_bias)
-        #self.mnist_28x28_conv = mnist_28x28_conv
         self.image_length = image_length
         self.image_width = image_width
 
@@ -214,7 +211,6 @@ class DiscriminatorNetCovid(CompetetiveNet):
             copy.deepcopy(self.net),
             self.data_size,
             self.optimize_bias,
-            # mnist_28x28_conv=self.mnist_28x28_conv,
             image_length=self.image_length,
             image_width=self.image_width
         )
@@ -230,24 +226,18 @@ class DiscriminatorNetCovid(CompetetiveNet):
         # Second term of the loss is always zero since real_labels == 1
         batch_size = input.size(0)
 
-        #if self.mnist_28x28_conv:
-        #    input = input.view(-1, 1, 28, 28)
         input = input.view(-1, 1, self.image_length, self.image_width)
-        print('Real images: {}'.format(input.size()))
 
         real_labels = to_pytorch_variable(torch.ones(batch_size))
         fake_labels = to_pytorch_variable(torch.zeros(batch_size))
 
         outputs = self.net(input).view(-1)
-        print('Prediction: {}'.format(outputs.size()))
         d_loss_real = self.loss_function(outputs, real_labels)
 
         # Compute loss using fake images
         # First term of the loss is always zero since fake_labels == 0
         z = noise(batch_size, self.data_size)
         fake_images = opponent.net(z)
-        print('Fake iamges: {}'.format(fake_images.size()))
-        print('G network: {}'.format(opponent.net))
         outputs = self.net(fake_images).view(-1)
         d_loss_fake = self.loss_function(outputs, fake_labels)
 
